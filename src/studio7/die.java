d053ea0d54b40590aa971448801a10976d29b4a5
alexandergadin <alexandergadin@localhost>
2022-11-02 16:56:14 -0500

    oi

--- src/studio7/die.java
+++ src/studio7/die.java
@@ -2,4 +2,15 @@ package studio7;
 
 public class die {
 
+	private int n;
+	
+	
+	//constructor
+	public die(int sides)  {
+		n=sides;
+	}
+	
+	public int thrown(int sides) {
+		return 1+(int) Math.random()*n;
+	}
 }
